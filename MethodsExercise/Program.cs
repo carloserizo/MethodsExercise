0d178331e1992c1e807957f0c1bb517c9f09da00
Mona Lisa <carlos.e.rizo@outlook.com>
2023-01-09 05:03:33 -0800

    math operator

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,8 +2,23 @@
 {
     public class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
         static void Main(string[] args)
         {
+
+            var amountOfCars = Sum(2, 6);
+            var blah = Multiply(60, 2, 4);
+
             //---Exercise 1---
             //Name: Michael
             //Favorite Color: Blue
@@ -29,6 +44,8 @@
             Console.WriteLine($"Favorite color: {color}");
             Console.WriteLine($"Favorite animal: {animal}");
             Console.WriteLine($"Favorite band: {band}");
+
+            //-----Exercise 2-----
 
         }
     }
